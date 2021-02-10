ba5d271ddfaf69522d87e4574fd1955a9e66ea02
Brian James Celine <brianceline@gmail.com>
2021-02-10 18:01:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+'''
+For in loop
+'''
+# students = ['Bob', 'Adriana', 'Felix']
+
+# for x in students:
+#   print('hi', x)
+
+# =============================
+
+# print(range(10))
+# print(list(range(0,10)))
+# print(list(range(3,6)))
+# print(list(range(3, 13, 3)))
+
+# =============================
+
+# for x in range(6):
+#   print(x + 1)
+
+# for i in range(1, 6):
+#   for j in range(i):
+#     print("*", end = " ")
+#   print()
+
+'''
+While loop
+'''
+
+# i = 1
+# while i < 10:
+#   print(i)
+#   i += 1
+
+# =============================
+# break statements
+
+# i = 1
+# while i < 10:
+#   print(i)
+#   if i == 5:
+#     break
+#   i += 1
+
+# =============================
+# continue statements
+
+# i = 1
+# while i < 5:
+#   i += 1
+#   if i == 4:
+#     continue
+#   print(i)
+
+  #============================
+
+# for i in range(1, 10):
+#   if i == 3:
+#     break
+#   print(i)
+
+#============================
+
+
+
+
+
+  
+  
